4164f454feec9375cfbfe9bf8df7838abbf49b5a
dmlyly <1401299350@qq.com>
2018-11-22 20:35:47 +0800

    2018-11-22 20:34 laravel7

---
+++ laravel7/storage/framework/views/e866d0ee232a3dacf4b5d290db5134a1a448d9ae.php
@@ -0,0 +1,7 @@
+<?php $__env->startSection('title',$title); ?>
+
+<?php $__env->startSection('content'); ?>
+的局面色块雷锋哥寂寞的人付款了国家每年可；、热节目；
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout.admins', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
